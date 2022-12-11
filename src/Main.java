7e625510a90c52beab58978631f72644637f64e9
YayCover9000 <yassuanf@googlemail.com>
2022-12-11 16:08:10 +0100

    Program for whole number till 1000

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,42 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Scanner sc = new Scanner(System.in);
-        int test = sc.nextInt();
+        int zahl = sc.nextInt();
+        boolean check = true;
+        int tausend=0;
+        int hundert=0;
+        int zehn=0;
+        int eins=0;
 
-        System.out.println(test);
+        //System.out.println(zahl);
+        while (check == true) {
+            if (zahl > 1000) {
+                tausend = zahl/1000;
+                System.out.print(tausend + " + ");
+                zahl = zahl - ((zahl / 1000) * 1000);
+
+            }
+            if (zahl > 100) {
+                hundert = zahl / 100;
+                System.out.print(hundert + " + ");
+                zahl = zahl - ((zahl / 100) * 100);
+
+            }
+            if (zahl > 10) {
+                zehn = zahl / 10;
+                System.out.print(zehn + " + ");
+                zahl = zahl - ((zahl / 10) * 10);
+
+            }
+            if (zahl > 1) {
+                eins = zahl / 1;
+                System.out.print(eins);
+                zahl = zahl - ((zahl / 1) * 1);
+            } else {
+                check = false;
+            }
+            System.out.println(" Quersumme entspricht = " + tausend+hundert+zehn+eins);
+
+        }
     }
 }
